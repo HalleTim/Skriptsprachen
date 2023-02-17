8681a9d48bc2b34c5d8a23d1c835688b3f44de96
Tim Halle <halletim@web.de>
2023-02-17 21:53:03 +0100

    s

--- titled-1.py
+++ titled-1.py
@@ -7,7 +7,7 @@ r=leds[0]
 g=leds[1]
 b=leds[2]
 
-np.put_along_axis(r,np.array([0]),rgb[0], axis=0)
+np.put_along_axis(r,np.array([0,1,2]),rgb[0], axis=0)
 np.put_along_axis(g,np.array([0]),rgb[1], axis=0)
 np.put_along_axis(b,np.array([0]),rgb[2], axis=0)
 
