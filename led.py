050c7f43b6770ab5e70b38b9614f733a0d99fe13
Tim Halle <halletim@web.de>
2023-02-18 13:58:46 +0100

    s

--- led.py
+++ led.py
@@ -63,7 +63,7 @@ class ledStrip:
         np.put_along_axis(b,leftLedsToShine,color[2], axis=0)
         
         self.leds=np.stack((r,g,b))
-        print(b)
+        print(self.leds)
         
         #self.leds=np.put(self.leds,rightLedsToShine,[color]*len(rightLedsToShine))
 
